894ca8eaeec6af4c042b1154a4920528342eb227
volos <88537879+rostislavvolos@users.noreply.github.com>
2022-05-12 01:13:52 +0300

    2 commit, bug with dash are destroyed

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const textField = document.getElementById("text")
-const wordCount = document.getElementById("wordCount")
-const clearBtn = document.getElementById("clearBtn")
+const textField = document.getElementById("text");
+const wordCount = document.getElementById("wordCount");
+const clearBtn = document.getElementById("clearBtn");
 
+textField.addEventListener("input", () => {
+  let txt = textField.value.trim().split(/[\s-]/);
+  // wordCount.textContent.split(' ').join('-');
+  wordCount.textContent = txt.filter((item) => item).length;
+  console.log(txt);
+});
 
-function countWord() {
-    let text = textField.value;
-    text = text.trim();
-    const words = text.split(" ")
-    if(words[0] === "") {
-        wordCount.innerText = 0;
-    } else {
-        wordCount.innerText = words.length;
-    }
-}
+//   function dashBug() {
+//     let txt = textField.value.trim();
+//   }
 
-
-clearBtn.onclick = ()=> {
-    textField.value = "";
-    countWord();
-}
+clearBtn.addEventListener("click", () => {
+  textField.value = "";
+  wordCount.textContent = 0;
+});
